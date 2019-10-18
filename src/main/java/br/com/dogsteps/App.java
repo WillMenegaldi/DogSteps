1a79f099e1ff8a6d47a6bdaa8c96b81a8611d2ea
João Santos <48814102+JoaoLourencoSantos@users.noreply.github.com>
2019-10-18 08:39:50 -0300

    Ignore

--- src/main/java/br/com/dogsteps/App.java
+++ src/main/java/br/com/dogsteps/App.java
@@ -1,5 +1,6 @@
 package br.com.dogsteps;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 import org.glassfish.grizzly.http.server.HttpServer;
@@ -7,23 +8,23 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class App {
-	private static final int porta = 8080;
-	private static final String BASE_URI = "http://localhost:" + porta + "/dogsteps";
+	private static final String PACKAGE = "br.com.dogsteps";
+	private static final int PORTA = 8080;
+	private static final String BASE_URI = "http://localhost:" + PORTA + "/dogsteps";
+	private static HttpServer server;
+	private static ResourceConfig resourceConfig;
 
 	public static void main(String[] args) throws IOException {
-		final HttpServer server = startServer();
-
+		server = startServer();
 		System.out.println(String.format(
-				"Aplicação de está iniciada na porta " + porta + "\nPressione enter para terminar...", BASE_URI));
-
+				"Aplicação de está iniciada na porta " + PORTA + "\nPressione enter para terminar...", BASE_URI));
 		System.in.read();
-
 		server.shutdownNow();
 	}
 
 	public static HttpServer startServer() {
-		final ResourceConfig rc = new ResourceConfig().packages("br.com.dogsteps");
-		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+		resourceConfig = new ResourceConfig().packages(PACKAGE);
+		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
 	}
 
 }
