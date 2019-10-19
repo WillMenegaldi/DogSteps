b022c39f56e18b20e2e9c4f1528bbdd6b04d5b77
andrewcs0901 <andrewcs0901@gmail.com>
2019-10-19 17:00:23 -0300

    commit: base_repositories & PetDAO

---
+++ src/main/java/br/com/dogsteps/repositories/DogwalkerRepository.java
@@ -0,0 +1,20 @@
+package br.com.dogsteps.repositories;
+
+import br.com.dogsteps.dao.DogwalkerDAO;
+import br.com.dogsteps.models.Dogwalker;
+
+import java.util.List;
+
+public class DogwalkerRepository {
+
+	private static DogwalkerDAO dogwalker;
+
+	public List<Dogwalker> getList() {
+		return null;
+	}
+
+	public Dogwalker getDogwalker(int id) {
+		return null;
+	}
+
+}
