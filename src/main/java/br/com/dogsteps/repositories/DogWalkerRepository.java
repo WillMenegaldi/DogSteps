1dff64cec698b30e84f7a0c4cb5fa145aef64e23
João Guilherme <juaogui159@gmail.com>
2019-10-22 13:27:36 -0300

    DogWalker - Repository and Service with the new Arquiteture

--- src/main/java/br/com/dogsteps/repositories/DogWalkerRepository.java
+++ src/main/java/br/com/dogsteps/repositories/DogWalkerRepository.java
@@ -1,20 +1,57 @@
 package br.com.dogsteps.repositories;
 
-import br.com.dogsteps.dao.DogWalkerIDAO;
-import br.com.dogsteps.models.DogWalker;
-
+import java.io.IOException;
 import java.util.List;
 
-public class DogWalkerRepository {
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
-	private static DogWalkerIDAO dogwalker;
+import br.com.dogsteps.dao.Dao;
+import br.com.dogsteps.interfaces.IRepository;
+import br.com.dogsteps.models.DogWalker;
+import br.com.dogsteps.models.Tour;
 
-	public List<DogWalker> getList() {
+public class DogWalkerRepository implements IRepository<DogWalker, String> {
+
+	private static final String FILE_NAME = "database/dogwalker";
+	private final Dao DOGWALKER_DAO = inicializarDao();
+
+	public Dao inicializarDao() {
+		try {
+			return new Dao(FILE_NAME);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
-	public DogWalker getDogwalker(int id) {
-		return null;
+	public List<DogWalker> getList() {
+		return DOGWALKER_DAO.getAll();
+	}
+
+	public DogWalker find(String id) {
+		return (DogWalker) DOGWALKER_DAO.get(id);
+	}
+
+	public Response add(DogWalker dogWalker) {
+		if (DOGWALKER_DAO.add(dogWalker))
+			return Response.status(Status.OK).build();
+		else
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+	}
+
+	public Response update(DogWalker dogWalker) {
+		if (DOGWALKER_DAO.update(dogWalker))
+			return Response.status(Status.OK).build();
+		else
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+	}
+
+	public Response remove(String id) {
+		if (DOGWALKER_DAO.remove(id))
+			return Response.status(Status.OK).build();
+		else
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
 	}
 
 }
