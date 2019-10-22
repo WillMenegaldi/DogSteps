99a83337ec5004a04003d146c4149eb8de80d6d1
João Santos <48814102+JoaoLourencoSantos@users.noreply.github.com>
2019-10-22 20:47:59 -0300

    Modificações em estruturas antigas

--- src/main/java/br/com/dogsteps/repositories/PetRepository.java
+++ src/main/java/br/com/dogsteps/repositories/PetRepository.java
@@ -1,28 +1,5 @@
 package br.com.dogsteps.repositories;
 
-import br.com.dogsteps.models.Pet;
-
-import java.util.List;
-
 public class PetRepository {
 
-	private static PetIDAO pet;
-
-	public List<Pet> getList() {
-		return pet.getAll();
-	}
-
-	public Pet getPet(int id) {
-		return pet.get(id);
-	}
-
-    public void savePet(Pet p) { pet.add(p);}
-
-	public boolean removePet(Pet p) {
-		return false;
-	}
-
-	public boolean update(Pet p) {
-		return pet.update(p);
-	}
 }
--- src/main/java/br/com/dogsteps/repositories/RatingRepository.java
+++ src/main/java/br/com/dogsteps/repositories/RatingRepository.java
@@ -4,11 +4,4 @@ import br.com.dogsteps.models.Rating;
 import java.util.List;
 
 public class RatingRepository {
-
-	private static RatingIDAO rating;
-
-	public List<Rating> getList() {
-		return null;
-	}
-
 }
