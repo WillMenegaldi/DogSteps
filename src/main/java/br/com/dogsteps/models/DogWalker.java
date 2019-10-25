917c8d8b594d1b9852f8cf3ab2d33719f8f3d10a
João Santos <48814102+JoaoLourencoSantos@users.noreply.github.com>
2019-10-25 08:57:26 -0300

    Refatoração

--- src/main/java/br/com/dogsteps/models/DogWalker.java
+++ src/main/java/br/com/dogsteps/models/DogWalker.java
@@ -3,12 +3,9 @@ package br.com.dogsteps.models;
 import java.util.ArrayList;
 
 public class DogWalker extends User {
-
-	
 	private ArrayList<Avaliacao> avaliacoes;
 	private double mediaAvaliacao;
 	private String descriçao;
-
 
 	public DogWalker(String nome, String photoUrl, int idade, String cpf, String email, String senha, Endereco endereco,
 			Agenda agenda, ArrayList<Avaliacao> avaliacoes, double mediaAvaliacao, String descriçao) {
@@ -16,37 +13,28 @@ public class DogWalker extends User {
 		this.avaliacoes = avaliacoes;
 		this.mediaAvaliacao = mediaAvaliacao;
 		this.descriçao = descriçao;
-	}	
+	}
 
 	public ArrayList<Avaliacao> getAvaliacoes() {
 		return avaliacoes;
 	}
-
-
 	public void setAvaliacoes(ArrayList<Avaliacao> avaliacoes) {
 		this.avaliacoes = avaliacoes;
 	}
 
-
 	public double getMediaAvaliacao() {
 		return mediaAvaliacao;
 	}
-
-
 	public void setMediaAvaliacao(double mediaAvaliacao) {
 		this.mediaAvaliacao = mediaAvaliacao;
 	}
 
-
 	public String getDescriçao() {
 		return descriçao;
 	}
-
-
 	public void setDescriçao(String descriçao) {
 		this.descriçao = descriçao;
 	}
-
-
+	
 	public DogWalker(){}
 }
