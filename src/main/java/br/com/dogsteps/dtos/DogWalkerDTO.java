32a28257b1d3864f5392cf64b3b5f75318f28c25
João Santos <48814102+JoaoLourencoSantos@users.noreply.github.com>
2019-10-27 18:28:47 -0300

    Adição da classe preferencias

---
+++ src/main/java/br/com/dogsteps/dtos/DogWalkerDTO.java
@@ -0,0 +1,11 @@
+package br.com.dogsteps.dtos;
+
+import br.com.dogsteps.enums.EPort;
+import br.com.dogsteps.models.Endereco;
+
+import java.time.LocalDate;
+
+public class DogWalkerDTO {
+    Endereco endereco;
+    EPort porte;
+}
